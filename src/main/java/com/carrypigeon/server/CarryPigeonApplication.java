d41ac2405a786245aa6796bea22f2e98ac2fb746
Shirasawa <shirasawa@localhost.localdomain>
2025-01-24 18:56:00 +0800

    init

---
+++ src/main/java/com/carrypigeon/server/CarryPigeonApplication.java
@@ -0,0 +1,13 @@
+package com.carrypigeon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryPigeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryPigeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carrypigeon/server/CarryPigeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrypigeon.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarryPigeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
