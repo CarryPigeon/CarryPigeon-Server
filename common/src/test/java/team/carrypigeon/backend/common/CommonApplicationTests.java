f9e4b4e9f27b0afa95801cff797e7ab661895392
midreamsheep <midreamsheep@gmail.com>
2025-08-10 15:11:27 +0800

    feat:命名规范化&自定义打包功能实现

---
+++ common/src/test/java/team/carrypigeon/backend/common/CommonApplicationTests.java
@@ -0,0 +1,11 @@
+package team.carrypigeon.backend.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class CommonApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
